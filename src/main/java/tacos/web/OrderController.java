7bf0d1646c58ca53a6cc772755f59c97f77b424b
Zayne <star1582@naver.com>
2023-08-26 10:38:16 +0100

    Handling a taco order submission

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,10 +2,13 @@ package tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
 
 @Slf4j
 @Controller
@@ -16,5 +19,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
     }
 }
