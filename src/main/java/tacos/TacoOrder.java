1e6a86759423f939a780e4d72e2743830babc782
Zayne <star1582@naver.com>
2023-08-28 11:47:10 +0100

    Validating a POSTed Taco

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -3,18 +3,39 @@ package tacos;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import lombok.Data;
 
 @Data
 public class TacoOrder {
 
+    @NotBlank(message = "Delivery name is required")
     private String deliveryName;
+
+    @NotBlank(message = "Street is required")
     private String deliveryStreet;
+
+    @NotBlank(message = "City is required")
     private String deliveryCity;
+
+    @NotBlank(message = "State is required")
     private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
     private String deliveryZip;
+
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,7 +70,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco : {}", taco);
 
