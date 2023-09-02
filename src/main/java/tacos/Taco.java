c9255551afcba8cff8ccd5e259864775777a606d
Zayne <star1582@naver.com>
2023-09-02 15:43:39 +0100

    Adding ID and timestamp fields

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -9,6 +10,10 @@ import lombok.Data;
 
 @Data
 public class Taco {
+
+    private Long id;
+
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
