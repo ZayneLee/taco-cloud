231268c2d20758b04e4a68b28f6cf53eb1345e3d
Zayne <star1582@naver.com>
2023-12-03 10:52:44 +0000

    Remove comments

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,7 +1,6 @@
 package tacos.web;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,9 +19,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
-import tacos.data.IngredientRepository;
 import tacos.Taco;
 import tacos.TacoOrder;
+import tacos.data.IngredientRepository;
 
 @Slf4j
 @Controller
@@ -42,18 +41,6 @@ public class DesignTacoController {
     // attributes.
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-
-        // List<Ingredient> ingredients = Arrays.asList(
-        // new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-        // new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-        // new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-        // new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-        // new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-        // new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-        // new Ingredient("CHED", "Cheddar", Type.CHEESE),
-        // new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-        // new Ingredient("SLSA", "Salsa", Type.SAUCE),
-        // new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 
         Iterable<Ingredient> ingredients = ingredientRepo.findAll();
 
