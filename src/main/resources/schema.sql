24113749230589195a16baafb0cff7b826bb9446
Zayne <star1582@naver.com>
2023-12-04 11:31:11 +0000

    clean code

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -31,7 +31,5 @@ create table if not exists Ingredient (
     type varchar(10) not null
 );
 
-alter table Taco
-    add foreign key (taco_order) references Taco_Order(id);
-alter table Ingredient_Ref
-    add foreign key (ingredient) references Ingredient(id);
+alter table Taco add foreign key (taco_order) references Taco_Order(id);
+alter table Ingredient_Ref add foreign key (ingredient) references Ingredient(id);
