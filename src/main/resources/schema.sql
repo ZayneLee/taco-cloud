11df345fb0d33daa3bcc6853afc234f0de8b47e2
Zayne <star1582@naver.com>
2023-12-04 11:06:36 +0000

    Preloading the database with data.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,7 +12,7 @@ create table if not exists Taco_Order (
 );
 
 create table if not exists Taco (
-    id identity;
+    id identity,
     name varchar(50) not null,
     taco_order bigint not null,
     taco_order_key bigint not null,
