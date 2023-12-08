bb2ea460c0980dd48e60836712932003a2bbe886
Zayne <star1582@naver.com>
2023-12-08 10:45:10 +0000

    Preparing the Incredient class for persistence

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,15 +1,30 @@
 package tacos;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class Ingredient {
+@Table
+@NoArgsConstructor // Add a default constructor
+@AllArgsConstructor // Keep the all-args constructor for internal use
+public class Ingredient implements Persistable<String> {
 
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    @Override
+    public boolean isNew() {
+        return id == null; // Implement logic for determining if the entity is new
     }
 }
